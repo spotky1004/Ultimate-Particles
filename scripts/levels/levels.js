636bf173a06c061d8ddf762c432ee3445cb0a602
Spotky1004 <spotky1004@gmail.com>
2020-12-03 19:58:20 +0900

    fix level 2-1

--- scripts/levels/levels.js
+++ scripts/levels/levels.js
@@ -27,77 +27,78 @@ function level_11() {
 function level_12() {
   levelInit();
 
-  PillowX = 0;
-  PillowY = 0;
-  PillowDeg = 0;
-  levelFunctions = new Task([
-    {callback: function(){
-      //some functions here!
-    }, time: 0, activated: false},
-  ]);
+PillowX = 0;
+PillowY = 0;
+PillowDeg = 0;
+levelFunctions = new Task([
+  {callback: function(){
+    //some functions here!
+  }, time: 0, activated: false},
+]);
 
-  levelTasks = new Task([
-    {callback: function(){
+levelTasks = new Task([
+  {callback: function(){
 
 
-    }, time: 0, activated: false},
-  ]);
+  }, time: 0, activated: false},
+]);
 
-  levelLoop = setInterval( function () {
-    levelLoopCount++;
-    screenSettings.size = 1.04;
-      screenSizeSpan(1, 10, 30)
-      particles[`${levelLoopCount}`] = new Particle({'speed': 30, 'spanper': 30, 'speedI': 9, 'speedIType': 'span', 'color': '#600', 'size': [0.03,0.03], 'position': [Math.random()*2-1,1]})
+levelLoop = setInterval( function () {
+  levelLoopCount++;
+    particles[`${levelLoopCount}`] = new Particle({'speed': 30, 'spanper': 30, 'speedI': 9, 'speedIType': 'span', 'color': '#600', 'size': [0.03,0.03], 'position': [Math.random()*2-1,1]})
 
-      if (levelLoopCount > 1) {
-        PillowX = particles[`${levelLoopCount}` - 1].position[0]
-        PillowY = particles[`${levelLoopCount}` - 1].position[1]
-        PillowDeg = Math.random()*90
+    if (levelLoopCount > 1) {
+      PillowX = particles[`${levelLoopCount}` - 1].position[0]
+      PillowY = particles[`${levelLoopCount}` - 1].position[1]
+      PillowDeg = Math.random()*90
 
-        for (var i = 0; i < 4; i++) {
-          particles[`${levelLoopCount}with${i}`] = new Particle({'speed': 12, 'deg': PillowDeg + i*90, 'color': '#600', 'position': [PillowX,PillowY]})
-        }
-
-        if (levelLoopCount > 30 && levelLoopCount <= 120) {
-          for (var i = 0; i < 4; i++) {
-            particles[`${levelLoopCount}with${i}withTwo`] = new Particle({'speed': 12, 'deg': (PillowDeg + i*90 + 45)%360, 'color': '#600', 'position': [PillowX,PillowY]})
-          }
-        }
-        if (levelLoopCount > 60 && levelLoopCount <= 120) {
-          for (var i = 0; i < 8; i++) {
-            particles[`${levelLoopCount}with${i}withThree`] = new Particle({'speed': 15, 'deg': (PillowDeg + i*45)%360, 'color': '#600', 'position': [PillowX,PillowY]})
-            particles[`${levelLoopCount}with${i}withFour`] = new Particle({'speed': 18, 'deg': (PillowDeg + i*45)%360, 'color': '#600', 'position': [PillowX,PillowY]})
-          }
-          for (var i = 0; i < 4; i++) {
-            particles[`${levelLoopCount}with${i}withThree`] = new Particle({'speed': 15, 'deg': (PillowDeg + i*90+22.5)%360, 'color': '#600', 'position': [PillowX,PillowY]})
-          }
-        }
-        if (levelLoopCount > 120) {
-          for (var i = 0; i < 16; i++) {
-            particles[`${levelLoopCount}with${i}withThree`] = new Particle({'speed': 18, 'deg': (PillowDeg + i*22.5)%360, 'color': '#600', 'position': [PillowX,PillowY]})
-            particles[`${levelLoopCount}with${i}withFour`] = new Particle({'speed': 22, 'deg': (PillowDeg + i*22.5)%360, 'color': '#600', 'position': [PillowX,PillowY]})
-          }
-          for (var i = 0; i < 6; i++) {
-            particles[`${levelLoopCount}with${i}withThree`] = new Particle({'speed': 8, 'spanper': 60, 'speedI': 30, 'speedIType': 'span', 'deg': (PillowDeg + i*60)%360, 'color': '#006', 'position': [PillowX,PillowY]})
-          }
-        }
-
-        delete particles[`${levelLoopCount}` - 1]
+      for (var i = 0; i < 4; i++) {
+        particles[`${levelLoopCount}with${i}`] = new Particle({'speed': 12, 'deg': PillowDeg + i*90, 'color': '#600', 'position': [PillowX,PillowY]})
       }
-  }, tickSpeed*32);
 
-  particles['player'] = new Particle({'type': 'player', 'color': '#f00'});
-  particles['text'] = new Particle({'type': 'text', 'absSize': 0.2, 'text': 'boom!', 'color': '#c49b29', 'zIndex': 1})
-  levelTasks.activateAll();
+      if (levelLoopCount > 30 && levelLoopCount <= 120) {
+        for (var i = 0; i < 4; i++) {
+          particles[`${levelLoopCount}with${i}withTwo`] = new Particle({'speed': 12, 'deg': (PillowDeg + i*90 + 45)%360, 'color': '#600', 'position': [PillowX,PillowY]})
+        }
+      }
+      if (levelLoopCount > 60 && levelLoopCount <= 120) {
+        for (var i = 0; i < 8; i++) {
+          particles[`${levelLoopCount}with${i}withThree`] = new Particle({'speed': 15, 'deg': (PillowDeg + i*45)%360, 'color': '#600', 'position': [PillowX,PillowY]})
+          particles[`${levelLoopCount}with${i}withFour`] = new Particle({'speed': 18, 'deg': (PillowDeg + i*45)%360, 'color': '#600', 'position': [PillowX,PillowY]})
+        }
+        for (var i = 0; i < 4; i++) {
+          particles[`${levelLoopCount}with${i}withFive`] = new Particle({'speed': 15, 'deg': (PillowDeg + i*90+22.5)%360, 'color': '#600', 'position': [PillowX,PillowY]})
+        }
+      }
+      if (levelLoopCount > 120) {
+        for (var i = 0; i < 16; i++) {
+          particles[`${levelLoopCount}with${i}withSix`] = new Particle({'speed': 18, 'deg': (PillowDeg + i*22.5)%360, 'color': '#600', 'position': [PillowX,PillowY]})
+          particles[`${levelLoopCount}with${i}withSeven`] = new Particle({'speed': 22, 'deg': (PillowDeg + i*22.5)%360, 'color': '#600', 'position': [PillowX,PillowY]})
+        }
+        for (var i = 0; i < 6; i++) {
+          particles[`${levelLoopCount}with${i}withEight`] = new Particle({'speed': 8, 'spanper': 60, 'speedI': 30, 'speedIType': 'span', 'deg': (PillowDeg + i*60)%360, 'color': '#006', 'position': [PillowX,PillowY]})
+        }
+      }
+
+      delete particles[`${levelLoopCount}` - 1]
+    }
+}, tickSpeed*32);
+
+particles['player'] = new Particle({'type': 'player', 'color': '#f00'});
+particles['text'] = new Particle({'type': 'text', 'absSize': 0.2, 'text': 'boom!', 'color': '#c49b29', 'zIndex': 1})
+levelTasks.activateAll();
 }
-function level_31() {
+function level_13() {
   console.log(31);
 }
-function level_41() {
+function level_14() {
   console.log(41);
 }
-function level_51() {
+function level_15() {
   console.log(51);
+}
+function level_16() {
+
 }
 //level 2-1, made by Spotky1004
 function level_21() {
@@ -220,5 +221,8 @@ function level_24() {
 
 }
 function level_25() {
+
+}
+function level_26() {
 
 }
